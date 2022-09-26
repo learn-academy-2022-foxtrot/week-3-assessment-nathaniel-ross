ad550361b980a318807c8ef84b4636e29cd3728b
nathaniel-ross <nathaniel.rossloyo@gmail.com>
2022-09-26 00:30:26 -0700

    finished assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,6 @@
 // ASSESSMENT 3: Coding Practical Questions with Jest
 
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -14,6 +15,15 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
+describe("fibSequence", () => {
+  it("takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.", () => {
+      expect(fibSequence(fibLength1)).toEqual([1, 1, 2, 3, 5, 8])
+      expect(fibSequence(fibLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+  })
+})
+
+// Good error: "ReferenceError: fibSequence is not defined"
+
 const fibLength1 = 6
 // Expected output: [1, 1, 2, 3, 5, 8]
 
@@ -22,10 +32,42 @@ const fibLength2 = 10
 
 // b) Create the function that makes the test pass.
 
+// Create a function named fibSequence
+// Create two variables, num1 and num2 with a value of 1, and a new empty array called fibArr
+// For loop for our input number to iterate 
+// New variable called fibAdder to hold num1 + num2
+// Using .push to add num1 into our empty array fibArr
+// Update num variables after push
+// Finally return fibArr
+
+const fibSequence = (fibLength1) => {
+  let num1 = 1;
+  let num2 = 1;
+  let fibArr = [];
+
+  for (let i = 0; i < fibLength1; i++) {
+    let fibAdder = num1 + num2
+    fibArr.push(num1);
+    num1 = num2;
+    num2 = fibAdder;
+    
+  }
+  return fibArr
+}
+
 // --------------------2) Create a function that takes in an object and returns an array of the numbers sorted from least to greatest.
 // Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
 
 // a) Create a test with expect statements for each of the variables provided.
+
+describe("studySorter", () => {
+  it("takes in an object and returns an array of the numbers sorted from least to greatest.", () => {
+      expect(studySorter(studyMinutesWeek1)).toEqual([15, 15, 20, 30, 30, 60, 90])
+      expect(studySorter(studyMinutesWeek2)).toEqual([10, 15, 20, 45, 60, 65, 100])
+  })
+})
+// Good error: "ReferenceError: studySorter is not defined"
+
 
 const studyMinutesWeek1 = {
   sunday: 90,
@@ -49,11 +91,29 @@ const studyMinutesWeek2 = {
 }
 // Expected output: [10, 15, 20, 45, 60, 65, 100]
 
-// b) Create the function that makes the test pass.
+// Create function studySorter 
+// Use Object.values method to return array of the objects property values
+// Use .sort to sort the values and then use (a, b) => a - b to sort the values in ascending order
+
+const studySorter = (object) => {
+  return Object.values(object).sort((a, b) => a - b)
+  }
+
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+describe("accumulatingArray", () => {
+  it("takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.", () => {
+      expect(accumulatingArray(accountTransactions1)).toEqual([100, 83, 60, 51])
+      expect(accumulatingArray(accountTransactions2)).toEqual([250, 161, 261, 165])
+      expect(accumulatingArray(accountTransactions3)).toEqual([])
+
+  })
+})
+// Good error: "ReferenceError: accumulatingArray is not defined"
+
 
 const accountTransactions1 = [100, -17, -23, -9]
 // Expected output: [100, 83, 60, 51]
@@ -65,3 +125,25 @@ const accountTransactions3 = []
 // Expected output: []
 
 // b) Create the function that makes the test pass.
+
+// create function accumulatingArray
+// create if statement if array is empty to return empty array using arr.length strictly equal to 0 to return [], and an else for the rest
+// create new let conditional with an array holding first value [0]
+// create for loop to iterate after the first value i = 1 
+// sum the value of newArray
+// return newArray
+// realize that I should've named accumulatingArray something shorter and easier to type 😐 (I suppose I could change it now)
+
+const accumulatingArray = (arr) => {
+  if (arr.length === 0) {
+    return []
+  } else {
+  let newArray = [arr[0]];
+  for (let i = 1; i < arr.length; i++) {
+    newArray[i] = newArray[i - 1] + arr[i];
+  }
+  return newArray;
+}}
+
+// console.log(accumulatingArray(accountTransactions3))
+// function worked except for accountTransactions3, returned undefined. Realized I needed an if statement for an empty array so I added that before the for loop
--- interview-questions.md
+++ interview-questions.md
@@ -8,48 +8,48 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
-Your answer:
+Your answer: JSX is a way to impliment HTML in a JavaScript file, JSX uses camelCase for "className" instead of HTML's "class". Another syntax used in JSX is the empty fragments <></> used to enclose your components.
 
-Researched answer:
+Researched answer: JSX (JavaScript Syntax Extension) is an extension of JavaScript used in React, allowing you to impliment HTML within JavaScript. Instead of using the class attribute in HTML you would use className in JSX. For HTML an example would be <h1 class="example">Howdy!</h1> whereas in JSX you would use <h1 className="example">Howdy!</h1>
 
 2. What is yarn? What file(s) are modified in a React application when you run yarn?
 
-Your answer:
+Your answer: Yarn is a command used to create a React application, when running yarn it creates two extra iles package.json and yarn.lock
 
-Researched answer:
+Researched answer: Yarn (Yet Another Resource Negotiator) is a package manager created by Facebook. Yarn creates package.json in your React project directory as well as yarn.lock
 
 3. What is an anonymous function in JavaScript?
 
-Your answer:
+Your answer: An anonymous function is a function with no name that you still want to declare, it's created with ()
 
-Researched answer:
+Researched answer: Anonymous functions are functions without a name. You would often use them as arguments to other functions
 
 4. What is the difference between state values and props in React?
 
-Your answer:
+Your answer: Props (properties) are used in React to pull from different components you have stored in your React App. State values is how you access those props.
 
-Researched answer:
+Researched answer: Both props and state values are objects in JavaScript. Props are used to pass data from components and state values are controlled by the component itself.
 
 5. What is the DOM?
 
-Your answer:
+Your answer: The DOM is how you interact with the HTML elements inside of your React app
 
-Researched answer:
+Researched answer: DOM (Document Object Model) is the visual representation of code, it's the interface for HTML. 
 
 6. STRETCH: Which is the difference between a div and a span?
 
-Your answer:
+Your answer: To be honest I'm not sure I just know that both are used in HTML
 
-Researched answer:
+Researched answer: Div and span are used to group sections of code, however div is used for large sections, while span is used for smaller sections
 
 ## Looking Ahead: Terms for Next Week
 
-1. Object-oriented programming:
+1. Object-oriented programming: A programming method designed around objects and classes
 
-2. Ruby:
+2. Ruby: Ruby is a general purpose programming language used for back-end development. It is object-oriented 
 
-3. Implicit return:
+3. Implicit return: Implicit return is when a function is returned, but without using the return keyword. 
 
-4. Ruby blocks:
+4. Ruby blocks: Ruby blocks are anonymous functions passed into methods. Ruby blocks are enclosed in a do/end statement or in between curly braces
 
-5. Ruby hashes:
+5. Ruby hashes: Ruby hashes are collections of key-value pairs 
